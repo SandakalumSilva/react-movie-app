14f03271ab4d95d6c2ee2319779a36e3cda84b15
SandakalumSilva <sandakalum1996@gmail.com>
2025-01-26 07:39:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./components/Movies/Movies";
+import Actors from "./components/Actors/Actors";
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors/:id" element={<Actors />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
