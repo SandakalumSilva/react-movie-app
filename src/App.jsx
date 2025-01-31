5eac1d456d22a071a1cc036a2efd210c985c0b7e
SandakalumSilva <sandakalum1996@gmail.com>
2025-01-31 08:27:48 +0530

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Movies from "./components/Movies/Movies";
 import Actors from "./components/Actors/Actors";
+import NavBar from "./components/NavBar/NavBar";
+import MovieInformation from "./components/MovieInformation/MovieInformation";
+import Profile from "./components/Profile/Profile";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Movies />} />
-          <Route path="/actors/:id" element={<Actors />} />
-        </Routes>
+        <NavBar />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Routes>
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/" element={<Movies />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { useTheme } from "@mui/styles";
+import useStyles from "./sideStyles";
+import { Link } from "react-router-dom";
+
+const redLogo = "https://i.ibb.co/ft4skBS/logored.png";
+const blueLogo = "https://i.ibb.co/5K6vdzx/logoblue.png";
+
+const categories = [
+  {
+    label: "Popular",
+    value: "popular",
+  },
+  {
+    label: "Top Rated",
+    value: "top_rated",
+  },
+  {
+    label: "Upcoming",
+    value: "upcoming",
+  },
+];
+
+const demoCategories = [
+  {
+    label: "Comedy",
+    value: "comedy",
+  },
+  {
+    label: "Action",
+    value: "action",
+  },
+  {
+    label: "Horror",
+    value: "horror",
+  },
+];
+
+const Sidebar = ({ setMobileOpen }) => {
+  const theme = useTheme();
+  const classes = useStyles();
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          className={classes.image}
+          src={theme.palette.mode === "light" ? redLogo : blueLogo}
+          alt="Movie"
+        />
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {demoCategories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img src={redLogo} className={classes.genreImage} height={30} />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img src={redLogo} className={classes.genreImage} height={30} />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
